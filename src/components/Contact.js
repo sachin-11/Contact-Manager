d1d43a7936804db71dc7d340db80d8d1e9f60cf8
sachin singh <sachin.singh@antino.io>
2020-03-01 23:27:20 +0530

    Adding contact Manager using react

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+ class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Sachin singh</h4>
+                <ul>
+                    <li>Email:sachin.antino@lonaride.com</li>
+                    <li>Phone: 9834573812</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+export default Contact;
